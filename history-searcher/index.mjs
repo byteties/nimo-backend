6fcc9e28caee44e079ce9e52c25d7053d9366ac7
Chayangkoon <chayangkoondokhom@gmail.com>
2024-11-12 12:46:45 +1100

    update history lambda to integrate with api-gateway

--- history-searcher/index.mjs
+++ history-searcher/index.mjs
@@ -6,35 +6,42 @@ const client = new DynamoDBClient();
 const dynamo = DynamoDBDocument.from(client);
 
 const coinLogQuery = async (username, day = '') => {
-    const response = await dynamo.query({
-        TableName: COIN_SEARCH_LOG_TABLE,
-        KeyConditionExpression: 'username = :username and begins_with(sk, :sk)',
-        ExpressionAttributeValues: {
-          ":username": username,
-          ":sk": day
-        }
-      });
-    const { Items, Count, LastEvaluatedKey } = response
-    return { 
-        items: Items,
-        count: Count,
-        nextToken: LastEvaluatedKey
-    };
+  const response = await dynamo.query({
+    TableName: COIN_SEARCH_LOG_TABLE,
+    KeyConditionExpression: 'username = :username and begins_with(sk, :sk)',
+    ExpressionAttributeValues: {
+      ":username": username,
+      ":sk": day
+    }
+  });
+  const { Items, Count, LastEvaluatedKey } = response
+  return { 
+    items: Items,
+    count: Count,
+    nextToken: LastEvaluatedKey
   };
-  
+};
 
 export const handler = async (event) => {
-  const { username, day } = event
-  try {
-    const coinsHostory = await coinLogQuery(username, day);
-    return { 
-      body: coinsHostory,
-      statusCode: 200
-    }
-  } catch (err) {
+  const { headers, queryStringParameters } = event
+  const { username } = headers;
+  const { day } = queryStringParameters;
+  if(!username) {
     return {
       statusCode: 400,
-      body: err.message
+      body: "username is missing in header"
     }
+  }
+  if(!day) {
+    return {
+      statusCode: 400,
+      body: "day is missing in queryStringParameters"
+    }
+  }
+  try {
+    const coinsHostory = await coinLogQuery(username, day);
+    return coinsHostory
+  } catch (err) {
+    return err.message
   }
 };
